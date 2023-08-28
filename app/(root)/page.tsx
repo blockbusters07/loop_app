17aa64be39dd0df28e2e410f8f02c0d2e356b619
Shreeya Desai <shreeya.desai02@gmail.com>
2023-08-29 00:00:44 +0530

    Finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -27,7 +27,11 @@ async function Home({
 
   return (
     <>
-      <h1 className='head-text text-left'>Home</h1>
+      <h4 className="main-head text-center " >Connect To Alumni & Get Your Doubts Cleared</h4>
+      <div>
+        <span>" "</span>
+      </div>
+      <h1 className='head-text text-center'>Home</h1>
 
       <section className='mt-9 flex flex-col gap-10'>
         {result.posts.length === 0 ? (
